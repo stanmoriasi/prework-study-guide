5588bbaf97caf0d0564685574a757a2adf5f3eec
stanmoriasi <stanmoriasi@yahoo.com>
2024-09-05 15:54:45 -0600

    add javascript function to randomly select a topic

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,35 @@
-const topics = ["HTML", "CSS", "Git", "JavaScript"];
-console.log(topics)
+const topic = ["HTML", "CSS", "Git", "JavaScript"];
+const randomTopic = topic[Math.floor(Math.random() * topic.length)];
+
+// console.log(topic)
 
 // const topic = "HTML";
 
-// if (topic === 'HTML') {
-//   console.log("Let's study HTML!");
-// } else if (topic === 'CSS') {
-//   console.log("Let's study CSS!");
-// } else if (topic === 'Git') {
-//   console.log("Let's study Git!");
-// } else if (topic === 'JavaScript') {
-//   console.log("Let's study JavaScript!");
-// } else {
-//   console.log('Please try again!');
-// }
 
-for (let x = 0; x<topics.length; x++)
-{
-    console.log(topics[x])
+
+
+function listTopics() {
+    for (let x = 0; x < topic.length; x++) {
+        console.log(topic[x])
+    }
 }
+
+function selectTopic() {
+
+    if (randomTopic === 'HTML') {
+        console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+        console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+        console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+    } else {
+        console.log('Please try again!');
+    }
+}
+console.log('Here are the topics we learned through Prework:');
+listTopics()
+
+console.log('Which topic should we study first?');
+selectTopic()
